d356bb5ade67c4f48c900ea4ee09550f5c6221aa
Phdoflip <antrodossantos@gmail.com>
2025-08-20 11:13:46 -0300

    inicial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="layout">
+      <aside className="sidebar">Menu lateral</aside>
+      <main className="content">Conteúdo principal</main>
+    </div>
+  );
+}
